fbf7a3561b2ff598c2fe676beab6ce32e40c08f7
Sagar Dobariya <infodsagar@gmail.com>
2022-08-02 20:11:28 +1000

    08

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
+//Get all products
 router.get('/', (req, res) => {
   res.json({ msg: 'hi' });
+});
+
+//Get a single product
+router.get('/:id', (req, res) => {
+  res.json({ msg: 'hi single' });
+});
+
+//Add product
+router.post('/', (req, res) => {
+  res.json({ msg: 'hi add' });
+});
+
+//Delete product
+router.delete('/:id', (req, res) => {
+  res.json({ msg: 'hi delete' });
 });
 
 module.exports = router;
